9265564460c064e9e567532ecc63e25ffff08c66
alejandrosamarin <alejandrosamarin@gmail.com>
2011-07-04 23:50:06 +0000

    Avanzando con la estructura de los filtros y el CUDAHandler

--- src/CUDAHandler.py
+++ src/CUDAHandler.py
@@ -1,25 +1,52 @@
+#!/opt/python2.7/bin/python
+
+import re
 import numpy as np
+
 import pycuda.autoinit
 import pycuda.driver as cuda
 from pycuda.compiler import SourceModule
 
 class CUDAHandler:
 
-    # Returns a GPU array
-    def _createGPUArray(a):
-        try:
-            data = np.array(a).astype(np.float32)
-        except ValueError:
-            print "Invalid list.",
-            print "All of its items must be float of integer."
-            break
-        finally:
-            data_gpu = cuda.mem_alloc(data.nbytes)
-            cuda.memcpy_htod(data_gpu, data)
-            return data_gpu
+    data_host = None
+    data_cuda = None
+    kernel = None
+    func_name = ""
 
-def copyToGPU(a):
-try:
-   createGPUArray(a)
-except:
-raise "FATAL ERROR: GPU array couldn't be create
+    def __init__(self):
+        pass
+
+    def copyToGPU(self, a):
+        try:
+            self.data_host = np.array(a).astype(np.float32)
+        except ValueError:
+            print "[CUDAHandler Error] Invalid datatype: All items must be integer or float"
+            raise
+        else:
+            self.data_cuda = cuda.mem_alloc(self.data_host.nbytes)
+            cuda.memcpy_htod(self.data_cuda, self.data_host)
+
+    def getFromGPU(self):
+        processed_data = np.zeros_like(self.data_host)
+        cuda.memcpy_dtoh(processed_data, self.data_cuda)
+        return processed_data 
+
+    def setKernel(self, kernel_str):
+        self.kernel = SourceModule(kernel_str)
+        regex = re.compile(r"\s*__global__\s+\w+\s+(\w+)")
+        match = regex.search(kernel_str)
+        try:
+            self.func_name = match.groups()[0]
+        except AttributeError:
+            print "[CUDAHandler Error] Could not retrieve main kernel function name"
+            raise
+
+    # TODO Pasar por argumento un **kwargs directamente, serian:
+    #      - hilos por bloque
+    #      - numero de bloques (bloques por grid)
+    #      - shape del bloque (tupla (x, y, z))
+    #      - shape del grid (tupla (x, y))
+    # Mirar lo de prepared_call() en vez de get_function()
+    def Launch(self, threads_per_block, nblocks):
+        func = self.kernel.get_function(self.func_name)
--- src/filter.py
+++ src/filter.py
@@ -4,6 +4,8 @@ import sys
 import Image
 import cProfile
 from abc import ABCMeta, abstractmethod
+
+import CUDAHandler
 from profiling import Profile
 
 # --------------------------------------- #
@@ -12,11 +14,18 @@ class Filter:
     """ Clase padre de filtros """
     __metaclass__ = ABCMeta
 
+    # CUDAHandler
+    cuda = None
+
     # Constantes para indicar con que dispositivo se debe procesar el filtro
     CPU = 0
     CUDA = 1
     OPENCL = 2
 
+    # Diccionario para mapear los modos de procesamiento (CPU, GPU, ...) con
+    # los respectivos metodos abstractos que definiran posteriormente los filtros
+    processing_method = {} 
+        
     # Atributos
     images = []
     post_img = None
@@ -24,6 +33,15 @@ class Filter:
     def __init__(self, *images):
         for im in images:
             self.images.append(im)
+        self.cuda = CUDAHandler()
+
+        # El diccionario de metodos se inicializa en el constructor ya que
+        # daba problemas crearlo directamente en los atributos
+        self.processing_method = { 
+            Filter.CPU    : self._processCPU,
+            Filter.CUDA   : self._processCUDA,
+            Filter.OPENCL : self._processCPU
+        }
 
     # TODO Esquema de colores como parametro
     def newPostImg(self, mode, size):
@@ -31,6 +49,14 @@ class Filter:
 
     def fetchResult(self):
         return self.post_img
+
+    @abstractmethod
+    def _processCPU(self):
+        pass
+
+    @abstractmethod
+    def _processCUDA(self):
+        pass
 
     @abstractmethod
     def Apply(self):
@@ -42,27 +68,44 @@ class ErosionFilter(Filter):
     def __init__(self, *images):
         super(ErosionFilter, self).__init__(*images)
         
+    def _processCPU(self):
+        pass
+
+    def _processCUDA(self):
+        pass
+
     def Apply(self):
         pass
 
 # --------------------------------------- #
 
 class DifferenceFilter(Filter):
+    
     def __init__(self, *images):
         super(DifferenceFilter, self).__init__(*images)
 
-    # @Profile
-    def Apply(self, mode):
-        self.newPostImg(self.images[0].mode, 
-                        (self.images[0].size[0], self.images[0].size[1]))
+    def _processCPU(self):
         for x in xrange(self.images[0].size[0]):
             for y in xrange(self.images[0].size[1]):
                 # "diff" resultara ser una tupla de 3 elementos (en imagenes RGB) con la 
                 # diferencia en valor absoluto por cada canal en ese pixel, comparado con el
                 # mismo pixel de la imagen anterior
-                diff = tuple([abs(a - b) for a,b in zip(self.images[0].getpixel((x, y)), self.images[1].getpixel((x, y)))])
+                diff = tuple([abs(a - b) for a,b in 
+                              zip(self.images[0].getpixel((x, y)), self.images[1].getpixel((x, y)))])
                 # img.putpixel((x, y), value)
                 self.post_img.putpixel((x, y), diff)
+
+    def _processCUDA(self):
+        pass
+
+    # @Profile
+    def Apply(self, mode):
+        # Se instancia una nueva imagen postprocesada, que sera la que contenga
+        # el resultado de haber aplicado el filtro a la/s imagen/es original/es
+        self.newPostImg(self.images[0].mode, (self.images[0].size[0], self.images[0].size[1]))
+        # Ahora se llama al metodo de procesado elegido
+        self.processing_method[mode]()
+
 
 # --------------------------------------- #
 
