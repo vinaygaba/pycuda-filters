8d551d4cacf28cf0c6ec6b3a3f3c609c689c5cbc
alejandrosamarin <alejandrosamarin@gmail.com>
2011-07-07 14:26:57 +0000

    Implementado filtro de erosion por CPU, anyadido kernel de erosion (falta pulirlo), retoques varios en filter.py

--- src/filter.py
+++ src/filter.py
@@ -3,6 +3,7 @@
 import sys
 import Image
 import cProfile
+import itertools
 from abc import ABCMeta, abstractmethod
 
 import CUDAHandler
@@ -14,6 +15,10 @@ class Filter:
     """ Clase padre de filtros """
     __metaclass__ = ABCMeta
 
+    # Constantes para los valores minimos y maximos de los pixeles
+    MAX_PIXEL_VALUE = 255
+    MIN_PIXEL_VALUE = 0
+    
     # CUDAHandler
     cuda = None
 
@@ -69,17 +74,13 @@ class Filter:
 
 class ThresholdFilter(Filter):
 
-    # Constantes para los valores minimos y maximos de los pixeles
-    MAX_PIXEL_VALUE = 255
-    MIN_PIXEL_VALUE = 0
-    
-    # Valor discriminante (menores que este -> 0; mayores que este -> 255)
-    threshold = 127
+    # Valor discriminante (menores que este -> MIN_PIXEL_VALUE mayores que este -> MAX_PIXEL_VALUE)
+    level = 127
 
     def __init__(self, *images, **kwargs):
         super(ThresholdFilter, self).__init__(*images)
         try:
-            self.threshold = kwargs['threshold']
+            self.level = kwargs['level']
         except KeyError:
             pass
         
@@ -87,7 +88,7 @@ class ThresholdFilter(Filter):
         # Modo "1" equivale a threshold con un valor discriminante estandar de 127
         # TODO comprobar si retorna una nueva instancia o si la aplica sobre la misma
         im = self.images[0].convert("1")
-        # self.images[0].point(lambda x: MAX_PIXEL_VALUE if x >= self.threshold else MIN_PIXEL_VALUE)
+        # self.images[0].point(lambda x: MAX_PIXEL_VALUE if x >= self.level else MIN_PIXEL_VALUE)
 
     def _processCUDA(self):
         pass
@@ -95,14 +96,93 @@ class ThresholdFilter(Filter):
 # --------------------------------------- #
 
 class ErosionFilter(Filter):
+    
+    # Mascara de aplicacion del filtro; contendra tuplas con las sumas que deben efectuarse
+    # a la posicion (x, y) del pixel actual que estamos tratando: por ejemplo, sumar (1, 0)
+    # significa (x + 1, y + 0), osea el pixel situado justo debajo, y por contra sumar (0, -1)
+    # implica (x + 0, y - 1), es decir el pixel situado justo a la izquierda del actual
+    mask = []
+
+    _kernel = """
+    #define   ROWS_BLOCKDIM_X 32
+    #define   ROWS_BLOCKDIM_Y 8
+    #define   ROWS_HALO_STEPS 1
+
+    __global__ void erosion_kernel(
+        float *d_Dst,
+        float *d_Src,
+        int imageW,
+        int imageH,
+        int pitch
+    ){
+        __shared__ float s_Data[ROWS_BLOCKDIM_Y][(2 * ROWS_HALO_STEPS) + ROWS_BLOCKDIM_X];
+
+        //Offset to the left halo edge
+        const int baseX = (blockIdx.x * ROWS_HALO_STEPS) * ROWS_BLOCKDIM_X + threadIdx.x;
+        const int baseY = blockIdx.y * ROWS_BLOCKDIM_Y + threadIdx.y;
+
+        d_Src += baseY * pitch + baseX;
+        d_Dst += baseY * pitch + baseX;
+
+        //Load main data
+        #pragma unroll
+        for(int i = ROWS_HALO_STEPS; i < ROWS_HALO_STEPS + ROWS_RESULT_STEPS; i++)
+            s_Data[threadIdx.y][threadIdx.x + i * ROWS_BLOCKDIM_X] = d_Src[i * ROWS_BLOCKDIM_X];
+
+        //Load left halo
+        #pragma unroll
+        for(int i = 0; i < ROWS_HALO_STEPS; i++)
+            s_Data[threadIdx.y][threadIdx.x + i * ROWS_BLOCKDIM_X] = (baseX >= -i * ROWS_BLOCKDIM_X ) ? d_Src[i * ROWS_BLOCKDIM_X] : 0;
+
+        //Load right halo
+        #pragma unroll
+        for(int i = 0; i < ROWS_HALO_STEPS; i++)
+            s_Data[threadIdx.y][threadIdx.x + i * ROWS_BLOCKDIM_X] = (imageW - baseX > i * ROWS_BLOCKDIM_X) ? d_Src[i * ROWS_BLOCKDIM_X] : 0;
+
+        //Compute and store results
+        __syncthreads();
+
+        #pragma unroll
+        for(int i = ROWS_HALO_STEPS; i < ROWS_HALO_STEPS; i++){
+            float result = 0;
+
+            #pragma unroll
+            for(int j = -KERNEL_RADIUS; j <= KERNEL_RADIUS; j++)
+                result = (s_Data[threadIdx.y][threadIdx.x + i * ROWS_BLOCKDIM_X + j] > 0) ? 255 : 0; 
+
+            d_Dst[i * ROWS_BLOCKDIM_X] = result;
+        }
+    }
+    """
+
     def __init__(self, *images):
         super(ErosionFilter, self).__init__(*images)
+        self.mask = [(i, j) for i, j in itertools.permutations([-1, 0, 1], 2) if abs(i) != abs(j)]
         
     def _processCPU(self):
-        pass
+        for row in xrange(self.images[0].size[0]):
+            for col in xrange(self.images[0].size[1]):
+                try:
+                    surrounding_pixels = [self.images[0].getpixel(([row + i, col + j])) for i, j in self.mask]
+                except IndexError:
+                    # Si se produjo alguna excepcion de indexado (debido a que estamos situados en algun borde
+                    # de la imagen), construimos la lista de pixels adyacentes replicando el pixel actual
+                    # TODO Intentar reconocer que pixel es el que tiro la excepcion, y replicar solo ese
+                    surrounding_pixels = list(itertools.repeat(self.images[0].getpixel((row, col)), len(self.mask)))
+                finally:
+                    # La funcion implicita "all()" devuelve True si _todos_ los elementos de un objeto iterable
+                    # son asimismo True (o distintos de 0 si los elementos son numericos, como es el caso)
+                    if all(surrounding_pixels):
+                        self.post_img.putpixel((row, col), 255)
+                    else:
+                        self.post_img.putpixel((row, col), 0)
 
     def _processCUDA(self):
-        pass
+        cuda.copyToGPU(self.images[0])
+        cuda.setKernel(self._kernel)
+        # TODO Dividir los grids atendiendo al ancho y alto de la imagen
+        # cuda.Launch(...)
+        cuda.getFromGPU()
 
 # --------------------------------------- #
 
@@ -112,6 +192,7 @@ class DifferenceFilter(Filter):
         super(DifferenceFilter, self).__init__(*images)
 
     def _processCPU(self):
+        # TODO Debe haber una implementacion mas eficiente utilizando numpy
         for x in xrange(self.images[0].size[0]):
             for y in xrange(self.images[0].size[1]):
                 # "diff" resultara ser una tupla de 3 elementos (en imagenes RGB) con la 
@@ -127,10 +208,17 @@ class DifferenceFilter(Filter):
 
 # --------------------------------------- #
 
-im1 = Image.open(sys.argv[1])
-im2 = Image.open(sys.argv[2])
-print sys.argv[1], ": ", im1.format, im1.size, im1.mode, '\n'
-diferencia = DifferenceFilter(im1, im2)
-diferencia.Apply(Filter.CPU)
-post = diferencia.fetchResult()
-post.save("post.png", "PNG")
+if __name__ == '__main__':
+    im1 = Image.open(sys.argv[1])
+    im2 = Image.open(sys.argv[2])
+    print sys.argv[1], ": ", im1.format, im1.size, im1.mode, '\n'
+    diferencia = DifferenceFilter(im1, im2)
+    diferencia.Apply(Filter.CPU)
+    tmp = diferencia.fetchResult()
+    # Despues del filtro de diferencia hay que aplicar Threshold
+    # ...
+    # Y despues del Threshold, Erosion
+    erosion = ErosionFilter(tmp)
+    erosion.Apply(Filter.CPU)
+    post = erosion.fetchResult()
+    post.save("post.png", "PNG")
