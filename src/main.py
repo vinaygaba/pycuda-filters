1bb97e72075aef789853e0bca71fabe2c746c000
alejandrosamarin <alejandrosamarin@gmail.com>
2011-07-13 01:06:23 +0000

    MotionDetector version CPU terminado, a falta de prueba final en garoe

--- src/main.py
+++ src/main.py
@@ -4,38 +4,16 @@ import sys
 import Image
 import ImageChops
 
+from motionDetector import MotionDetector
 from Filters.filter import Filter
 from Filters.erosion import ErosionFilter
 from Filters.difference import DifferenceFilter
 from Filters.threshold import ThresholdFilter
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 2:
         print 'Usage:', sys.argv[0], '<image1> <image2'
         sys.exit(1)
     
-    im1 = Image.open(sys.argv[1])
-    im2 = Image.open(sys.argv[2])
-    print sys.argv[1], ": ", im1.format, im1.size, im1.mode, '\n'
-
-    # Diferencia
-    diferencia = DifferenceFilter(im1, im2)
-    diferencia.Apply(Filter.CPU)
-    tmp = diferencia.fetchResult()
-
-    # Threshold
-    threshold = ThresholdFilter(tmp, level=30)
-    threshold.Apply(Filter.CPU)
-    tmp2 = threshold.fetchResult()
-
-    # Erosion
-    erosion = ErosionFilter(tmp2)
-    erosion.Apply(Filter.CPU)
-    post = erosion.fetchResult()
-
-    # TODO Mergeado en una clase aparte
-    r, g, b = im2.split()
-    tmp = ImageChops.add(r, post)
-    merged = Image.merge("RGB", (tmp, g, b))
-
-    merged.save("merged.png", "PNG")
+    motion = MotionDetector(sys.argv[1])
+    motion.launchCPU()
--- src/motionDetector.py
+++ src/motionDetector.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-import cv
+import Image
+import ImageChops
 
 from videoHandler import VideoHandler
 from Filters.filter import Filter
@@ -10,16 +11,43 @@ from Filters.threshold import ThresholdFilter
 
 class MotionDetector:
    
-   pre_video = None
-   post_video = None
+    pre_video = None
+    post_video = None
 
-   def __init__(self, video_path):
-      self.pre_video = VideoHandler(video_path)
-      self.post_video = VideoHandler()
+    def __init__(self, video_path):
+        self.pre_video = VideoHandler(video_path)
+        # TODO cambiar el constructor del videohandler
+        self.post_video = VideoHandler()
 
-   def LaunchCPU(self):
-      
+    def launchCPU(self):
+        frames_PIL = [Image.fromstring("RGB", self.pre_video.getSize(), i.tostring()) for i in self.pre_video.getAllFrames()]
+        for i in xrange(1, len(frames_PIL)):
+            # Diferencia
+            diferencia = DifferenceFilter(frames_PIL[i], frames_PIL[i + 1])
+            diferencia.Apply(Filter.CPU)
+            tmp = diferencia.fetchResult()
 
-   def LaunchCUDA(self):
-      pass
+            # Threshold
+            threshold = ThresholdFilter(tmp, level=20)
+            threshold.Apply(Filter.CPU)
+            tmp2 = threshold.fetchResult()
+
+            # Erosion
+            erosion = ErosionFilter(tmp2)
+            erosion.Apply(Filter.CPU)
+            post = erosion.fetchResult()
+
+            # TODO Mergeado en una clase aparte
+            r, g, b = frames_PIL[i + 1].split()
+            tmp = ImageChops.add(r, post)
+            merged = Image.merge("RGB", (tmp, g, b))
+
+            self.post_video.appendFrame(merged)
+            print "#",
+
+        # Terminado, salvamos resultado
+        self.post_video.Save("./out.mpg")
+    
+    def launchCUDA(self):
+        pass
 
